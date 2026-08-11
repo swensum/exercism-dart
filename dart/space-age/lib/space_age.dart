f2523d9c9ffd5f67804358dbbd58fab77763463c
Sumit <shresthaswen80@gmail.com>
2026-08-11 11:01:19 +0545

    new file update

---
+++ dart/space-age/lib/space_age.dart
@@ -0,0 +1,18 @@
+class SpaceAge {
+  double age({required String planet, required int seconds}){
+    const  secondsPerEarthYear = 31557600;
+    const orbitalPeriods ={
+      'Mercury': 0.2408467,
+      'Venus': 0.61519726,
+      'Earth': 1.0,
+      'Mars' : 1.8808158,
+      'Jupiter': 11.862615,
+      'Saturn': 29.447498,
+      'Uranus': 84.016846,
+      'Neptune': 164.79132,
+    };
+    final earthYears = seconds / secondsPerEarthYear;
+    final planetYears = earthYears / orbitalPeriods[planet]!;
+    return double.parse(planetYears.toStringAsFixed(2));
+  }
+}
