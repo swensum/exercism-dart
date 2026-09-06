c20477bc831f25ce757a943a68eeb5015afa0c07
Sumit <shresthaswen80@gmail.com>
2026-09-06 17:00:02 +0545

    adding new exercise

--- dart/queen-attack/lib/queen_attack.dart
+++ dart/queen-attack/lib/queen_attack.dart
@@ -1,3 +1,4 @@
+
 class Queen {
   final int row;
   final int column;
@@ -5,22 +6,24 @@ class Queen {
   Queen(this.row, this.column) {
     assert(row >= 0, 'row not positive');
     assert(row < 8, 'row not on board');
-    assert(column >= 0, 'column not on positive');
+
+    assert(column >= 0, 'column not positive');
     assert(column < 8, 'column not on board');
   }
+
   bool canAttack(Queen other) {
-    //same row 
     if (row == other.row) {
       return true;
     }
-    //same column
-    if(column == other.column){
+
+    if (column == other.column) {
       return true;
     }
-// same diagonal
-if ((row - other.row).abs() == (column - other.column).abs()){
-  return true;
-} 
-//None of the three conditions matched
-return false; }
+
+    if ((row - other.row).abs() == (column - other.column).abs()) {
+      return true;
+    }
+
+    return false;
+  }
 }
